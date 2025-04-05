f50d3f1d3157524bd7d0fffbf45fb052d7b97f43
Yagyesh-Jindal <133736229+Yagyesh-Jindal@users.noreply.github.com>
2025-04-05 09:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,47 @@
-provider "azurerm" {
-  features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-}
-
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# App Service Plan
-resource "azurerm_app_service_plan" "plan" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Basic"
-    size = "B1"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
   }
+  required_version = ">=1.0.0"
 }
 
-# App Service (Web App)
-resource "azurerm_app_service" "app" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+provider "azurerm" {
+  subscription_id = "07a89969-c27b-4e15-be71-d02064b89cef"
+  tenant_id       = "b2b0fca2-b9c0-4158-8704-f6cc4fed6adb"
+  features {}
+}
+
+resource "azurerm_resource_group" "web_rg" {
+  name     = "WebServiceRG"
+  location = "Central India"
+}
+
+resource "azurerm_service_plan" "Web_plan" {
+  name                = "webPlan01"
+  location            = azurerm_resource_group.web_rg.location
+  resource_group_name = azurerm_resource_group.web_rg.name
+  os_type             = "Windows"
+
+  sku_name = "B1"
+
+}
+resource "azurerm_app_service" "web_app" {
+  name                = "jecrc-yagyesh-rg"
+  location            = azurerm_resource_group.web_rg.location
+  resource_group_name = azurerm_resource_group.web_rg.name
+  app_service_plan_id = azurerm_service_plan.Web_plan.id
 
   site_config {
-    always_on = true
+    dotnet_framework_version = "v6.0" # Change to your preferred version
+    scm_type                 = "LocalGit"
   }
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "WEBSITE_RUN_FROM_PACKAGE" = "0"
   }
+
+  https_only = true
 }
